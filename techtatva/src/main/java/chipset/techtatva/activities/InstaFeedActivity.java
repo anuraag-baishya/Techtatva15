6590345665261d1fd5eaf09903f23239be6e1561
chipset <chipset95@yahoo.co.in>
2015-09-22 18:39:24 +0530

    Huge Code Refactoring

---
+++ techtatva/src/main/java/chipset/techtatva/activities/InstaFeedActivity.java
@@ -0,0 +1,129 @@
+package chipset.techtatva.activities;
+
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import chipset.techtatva.R;
+import chipset.techtatva.adapters.InstaFeedListAdapter;
+import chipset.techtatva.model.instagram.InstaFeed;
+import chipset.techtatva.network.APIClient;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+
+public class InstaFeedActivity extends AppCompatActivity {
+
+    private static final String TAG = InstaFeedActivity.class.getSimpleName();
+    private ListView mInstaFeedListView;
+    private ProgressBar mProgressBar;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadInstaFeed();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    public void loadInstaFeed() {
+        setContentView(R.layout.activity_insta_feed);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_r);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mInstaFeedListView = (ListView) findViewById(R.id.insta_feed_list_view);
+        mProgressBar = (ProgressBar) findViewById(R.id.insta_feed_progress_bar);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.insta_feed_swipe_refresh);
+
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.primary, R.color.primary_dark);
+
+        APIClient.getInstagram().getFeed(new Callback<InstaFeed>() {
+            @Override
+            public void success(InstaFeed instaFeed, Response response) {
+                mInstaFeedListView.setAdapter(new InstaFeedListAdapter(getApplicationContext(), instaFeed));
+                mInstaFeedListView.setVisibility(View.VISIBLE);
+                mProgressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                mProgressBar.setVisibility(View.GONE);
+                setContentView(R.layout.no_connection_layout);
+                Button retryButton = (Button) findViewById(R.id.retry_button);
+                retryButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        loadInstaFeed();
+                    }
+                });
+            }
+        });
+
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                mSwipeRefreshLayout.setRefreshing(true);
+                APIClient.getInstagram().getFeed(new Callback<InstaFeed>() {
+                    @Override
+                    public void success(InstaFeed instaFeed, Response response) {
+                        InstaFeedListAdapter adapter = new InstaFeedListAdapter(getApplicationContext(), instaFeed);
+                        adapter.notifyDataSetChanged();
+                        mInstaFeedListView.setAdapter(adapter);
+                        mInstaFeedListView.setVisibility(View.VISIBLE);
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        mProgressBar.setVisibility(View.GONE);
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        setContentView(R.layout.no_connection_layout);
+                        Button retryButton = (Button) findViewById(R.id.retry_button);
+                        retryButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                loadInstaFeed();
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        mInstaFeedListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem == 0 && visibleItemCount > 0 && mInstaFeedListView.getChildAt(0).getTop() >= 0)
+                    mSwipeRefreshLayout.setEnabled(true);
+                else mSwipeRefreshLayout.setEnabled(false);
+            }
+        });
+    }
+
+}
---
+++ techtatva/src/main/java/chipset/techtatva/resources/Constants.java
@@ -0,0 +1,24 @@
+package chipset.techtatva.resources;
+
+public class Constants {
+    public static String EVENT_CATEGORY_ID = "cat_id";
+    public static String EVENT_NAME = "event_name";
+    public static String EVENT_ID = "event_id";
+    public static String EVENT_DETAIL = "description";
+    public static String EVENT_MAX_TEAM_SIZE = "event_max_team_number";
+    public static String EVENT_LOCATION = "venue";
+    public static String EVENT_START_TIME = "start_time";
+    public static String EVENT_END_TIME = "end_time";
+    public static String EVENT_DATE = "date";
+    public static String EVENT_DAY = "day";
+    public static String EVENT_CONTACT_NAME = "contact_name";
+    public static String EVENT_CONTACT_NUMBER = "contact_number";
+
+    public static String URL_CATEGORIES = "http://api.techtatva.in/categories";
+    public static String URL_EVENTS = "http://api.techtatva.in/events";
+    public static String URL_SCHEDULE = "http://schedule.techtatva.in";
+    public static String URL_REGISTRATION = "http://register.techtatva.in";
+    public static String URL_ONLINE_EVENTS = "http://techtatva.in/onlineevents";
+
+    public static String INSTA_DATA = "instaData";
+}
