e9cf97dbe96fb75be333d739f06035a68bce71cc
chipset <chipset95@yahoo.co.in>
2015-09-26 16:38:35 +0530

    No event card

--- techtatva/src/main/java/chipset/techtatva/adapters/EventCardListAdapter.java
+++ techtatva/src/main/java/chipset/techtatva/adapters/EventCardListAdapter.java
@@ -29,6 +29,8 @@ public class EventCardListAdapter extends RecyclerView.Adapter<EventViewHolder> 
     private DBHelper dbHelper;
     private int day;
 
+    private boolean hasEvents = false;
+
     public EventCardListAdapter(Context c, ArrayList<Event> events, int day) {
         this.mContext = c;
         this.mEventList = new ArrayList<>();
@@ -51,80 +53,87 @@ public class EventCardListAdapter extends RecyclerView.Adapter<EventViewHolder> 
 
     @Override
     public EventViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View itemView = LayoutInflater.
-                from(viewGroup.getContext()).
-                inflate(R.layout.event_item, viewGroup, false);
+        View itemView = LayoutInflater.from(viewGroup.getContext()).
+                inflate(hasEvents ? R.layout.event_item : R.layout.no_event_card, viewGroup, false);
         return new EventViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(final EventViewHolder mHolder, int i) {
-        final Event event = mEventList.get(i);
-        mHolder.textName.setText(event.getEvent_name());
-        mHolder.textDate.setText(event.getDate());
-        mHolder.textLocation.setText(event.getLocation());
-        mHolder.textTime.setText(event.getStartTime() + " to " + event.getEndTime());
-        mHolder.textMaxSize.setText("Max participants per team: " + event.getEventMaxTeamNumber());
-        mHolder.textContact.setText("Contact: " + event.getContactName());
-        mHolder.textFav.setText("Favourite Event");
-        mHolder.textFav.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dbHelper.addToFavorites(event);
-            }
-        });
-        mHolder.eventInfoImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = mHolder.textName.getText().toString();
-                for (final Event e : mEventList) {
-                    if (e.getEvent_name().toLowerCase().equals(name.toLowerCase())) {
-                        TextView message = new TextView(mContext);
-                        message.setPadding(30, 30, 30, 30);
-                        message.setMovementMethod(LinkMovementMethod.getInstance());
-                        message.setText(Html.fromHtml(e.getDescription()));
-                        message.setMovementMethod(LinkMovementMethod.getInstance());
-                        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-                        builder.setTitle(name);
-                        builder.setCancelable(true);
-                        builder.setView(message);
-                        builder.setIcon(R.drawable.ic_action_about);
-                        builder.show();
-                        break;
+        if (hasEvents) {
+            final Event event = mEventList.get(i);
+            mHolder.textName.setText(event.getEvent_name());
+            mHolder.textDate.setText(event.getDate());
+            mHolder.textLocation.setText(event.getLocation());
+            mHolder.textTime.setText(event.getStartTime() + " to " + event.getEndTime());
+            mHolder.textMaxSize.setText("Max participants per team: " + event.getEventMaxTeamNumber());
+            mHolder.textContact.setText("Contact: " + event.getContactName());
+            mHolder.textFav.setText("Favourite Event");
+            mHolder.textFav.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dbHelper.addToFavorites(event);
+                }
+            });
+            mHolder.eventInfoImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String name = mHolder.textName.getText().toString();
+                    for (final Event e : mEventList) {
+                        if (e.getEvent_name().toLowerCase().equals(name.toLowerCase())) {
+                            TextView message = new TextView(mContext);
+                            message.setPadding(30, 30, 30, 30);
+                            message.setMovementMethod(LinkMovementMethod.getInstance());
+                            message.setText(Html.fromHtml(e.getDescription()));
+                            message.setMovementMethod(LinkMovementMethod.getInstance());
+                            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                            builder.setTitle(name);
+                            builder.setCancelable(true);
+                            builder.setView(message);
+                            builder.setIcon(R.drawable.ic_action_about);
+                            builder.show();
+                            break;
+                        }
                     }
                 }
-            }
-        });
-        mHolder.textContact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new AlertDialog.Builder(mContext)
-                        .setMessage("Call " + event.getContactName() + " ?")
-                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Potato.potate().Intents().callIntent(mContext, "+91" + event.getContactNumber());
-                            }
-                        })
-                        .setNegativeButton(android.R.string.no, null)
-                        .create().show();
-            }
-        });
-        mHolder.eventCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mHolder.descriptionLayout.getVisibility() == View.VISIBLE) {
-                    mHolder.descriptionLayout.setVisibility(View.GONE);
-                } else {
-                    mHolder.descriptionLayout.setVisibility(View.VISIBLE);
+            });
+            mHolder.textContact.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new AlertDialog.Builder(mContext)
+                            .setMessage("Call " + event.getContactName() + " ?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Potato.potate().Intents().callIntent(mContext, "+91" + event.getContactNumber());
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, null)
+                            .create().show();
                 }
-            }
-        });
+            });
+            mHolder.eventCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mHolder.descriptionLayout.getVisibility() == View.VISIBLE) {
+                        mHolder.descriptionLayout.setVisibility(View.GONE);
+                    } else {
+                        mHolder.descriptionLayout.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount() {
-        return mEventList.size();
+        if (mEventList.size() == 0) {
+            hasEvents = false;
+            return 1;
+        } else {
+            hasEvents = true;
+            return mEventList.size();
+        }
     }
 
     public void filterData(String query) {
